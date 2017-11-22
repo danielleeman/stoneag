3cb228c36ecf7a7aa5a769e3e2d605a1289ea3bc
danielleeman <daniel.leeman@gmail.com>
2017-11-21 21:14:14 -0600

    Get Razor Token

--- js/map.js
+++ js/map.js
@@ -1,4 +1,5 @@
 var map;
+
 function loadMapScenario() {
     var fieldGeo = [[46.65326757894689, -96.42025035713709],[46.652448813710876, -96.42023384116604],[46.65244575870562, -96.42093202940694],[46.653262927370356, -96.42127000481528]];
     var polyPoints = [];
@@ -16,3 +17,43 @@ function loadMapScenario() {
     });
     map.entities.push(polygon);
 }
+
+function getRazorToken(){
+  var request = {
+    "userName": $("#userName").val(),
+    "password": $("#password").val()
+  };
+  $.ajax({
+    url: baseUrl + '/Services/API/RazorTrackingApi.svc/Authenticate',
+    crossDomain: true,
+    type: 'POST',
+    contentType: "application/json;charset=utf-8",
+    cache: false,
+    datatype: 'json',
+    data: JSON.stringify(request),
+    dataFilter: function(data) {
+      var unwrapped = eval('(' + data + ')');
+      if (unwrapped.hasOwnProperty('d'))
+        return unwrapped.d;
+      else
+        return unwrapped;
+    },
+    success: function(result) {
+      if (result.ErrorDetail) {
+        alert(result.ErrorDetail.toString());
+      }
+
+      var token = result.Token.toString();
+      console.log(token);
+
+      $("#status").text(result.AuthenticationStatus.toString());
+      $("#token").text(result.Token.toString());
+      $("#tokenInput, #locationTokenInput").val(result.Token.toString());
+    },
+    error: function(response) {
+      console.log(response);
+    }
+  });
+};
+
+var razor = getRazorToken();
